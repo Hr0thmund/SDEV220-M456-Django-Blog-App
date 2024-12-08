a82095e8a3a066fa323d26f1a116d65444637672
Hr0thmund <lfawkes@ivytech.edu>
2024-12-08 10:20:41 -0500

    Update settings.py
    Removed secret key

--- mysite/settings.py
+++ mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-w7udk#e6cxjaig(@4fs2vql#b_$6)g*ps^ihxxj*kz(+_2m5#('
+SECRET_KEY = '[redacted]'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
